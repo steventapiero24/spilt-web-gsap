b5becd7ad86f2d9ab3a95e35f10eba86505efa02
steventapiero24 <steventapiero24@gmail.com>
2025-07-23 12:59:17 +0200

    config def anda correction modules

--- gsap-spylt/src/App.jsx
+++ gsap-spylt/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div className='border border-red-500 text-5xl'>App</div>
+    <div className='border border-red-500 text-5xl'>Apps</div>
   )
 }
 
